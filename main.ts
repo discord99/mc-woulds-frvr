76bb946d5127e4f9086897fd76214a385b0a7d0e
discord99 <82556977+discord99@users.noreply.github.com>
2022-06-26 19:37:31 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+})
